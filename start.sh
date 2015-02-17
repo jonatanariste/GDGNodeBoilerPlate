3f2ce7657272f531e3deddd6fedde146ad5ea1c7
Jorge Cano <jorgeucano@gmail.com>
2015-02-17 17:19:34 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Levantando la instancia de nodejs con nohup"
-nohup nodejs server.js --fe_ip 10.240.146.199 &
+nohup nodejs server.js --fe_ip PUT_YOUR_PRIVATE_IP_HERE &
 echo "La instancia se levanto correctamente"
