5933b8a191580634a60e52ed36418b81aa10da4c
Jorge Cano <jorgeucano@gmail.com>
2015-02-19 14:44:05 -0300

    new test

--- fork.sh
+++ fork.sh
@@ -1,7 +1,14 @@
+git checkout -b feature
+#  ( making changes ... )
+git commit -m "done with feature"
+
 # It's time to fork the repo!
-#git fork
-git remote add jorgeucano git://github.com/jorgeucano/PROYECTNAME.git
+git fork
+#(forking repo on GitHub...)
+#·→ git remote add YOUR_USER git://github.com/YOUR_USER/hub.git
+
 # push the changes to your new remote
-git push jorgeucano feature
+git push YOUR_USER feature
 # open a pull request for the topic branch you've just pushed
 git pull-request
+#→ (opens a text editor for your pull request message)
