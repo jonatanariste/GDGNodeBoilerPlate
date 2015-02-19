3807494718f051fe9a79b94ff5e3f4cf0b2b4506
Jorge Cano <jorgeucano@gmail.com>
2015-02-19 13:08:55 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 echo "Levantando la instancia de nodejs con nohup"
 nohup nodejs server.js --fe_ip PUT_YOUR_PRIVATE_IP_HERE &
 echo "La instancia se levanto correctamente"
