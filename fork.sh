c95ff2f4955fa0a40e45c8ad6100c19894150b10
Jorge Cano <jorgeucano@gmail.com>
2015-02-19 14:36:56 -0300

    test fork

---
+++ fork.sh
@@ -0,0 +1,7 @@
+# It's time to fork the repo!
+git fork
+git remote add jorgeucano git://github.com/jorgeucano/PROYECTNAME.git
+# push the changes to your new remote
+$ git push jorgeucano feature
+# open a pull request for the topic branch you've just pushed
+$ git pull-request
