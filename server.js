fb28a94a7e0ab337088db538f1f17575fe11585e
Jorge Cano <jorgeucano@gmail.com>
2015-02-19 12:49:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,16 @@
 
 	// configuration =================
 
-//    	app.use('/js', express.static(__dirname + '/js'));
 
-	app.use(morgan('dev')); 										// log every request to the console
-	app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
-	app.use(bodyParser.json()); 									// parse application/json
-	app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-	app.use(methodOverride());
+	app.use('/scripts', express.static(__dirname + '/script'));
+   	app.use('/styles', express.static(__dirname + '/styles'));
+   	app.use('/images', express.static(__dirname + '/images'));
+	app.use('/fonts', express.static(__dirname + '/fonts'));
+        app.use(morgan('dev'));                                                                                 // log every request to the console
+        app.use(bodyParser.urlencoded({'extended':'true'}));                    // parse application/x-www-form-urlencoded
+        app.use(bodyParser.json());                                                                     // parse application/json
+        app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+        app.use(methodOverride());
 
 	// routes ======================================================================
 
@@ -30,12 +33,8 @@
   		var file = __dirname + '/GDGNodeBoilerPlate.zip';
   		res.download(file); // Envio el file
 	});
-	app.use(express.static(path.join(__dirname, 'styles')));
-
-	app.use('styles', express.static(__dirname + 'styles'));
-	app.use('scripts', express.static(__dirname + 'scripts'));
-	app.use('images', express.static(__dirname + 'images'));
-	app.use('fonts', express.static(__dirname + 'fonts'));
+	
+	
 
 	// listen (start app with node server.js) ======================================
 	app.listen(8080, argv.fe_ip);
