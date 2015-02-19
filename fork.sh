861be2bad03a0f09d50eea88810b4d10ab3f5b80
Jorge Cano <jorgeucano@gmail.com>
2015-02-19 14:44:42 -0300

    Update fork.sh

--- fork.sh
+++ fork.sh
@@ -8,7 +8,7 @@ git fork
 #·→ git remote add YOUR_USER git://github.com/YOUR_USER/hub.git
 
 # push the changes to your new remote
-git push YOUR_USER feature
+git push jorgeucano feature
 # open a pull request for the topic branch you've just pushed
 git pull-request
 #→ (opens a text editor for your pull request message)
